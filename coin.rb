0e1808b8c9397a2c87bf6b11d91b88637684c2af
YYsvg <rara10202001@gmail.com>
2024-07-07 15:25:50 +0900

    create_api

---
+++ coin.rb
@@ -0,0 +1,23 @@
+require './method'
+
+get_my_money("BTC")["amount"]
+
+while(1)
+  current_price = get_price
+  puts current_price
+
+  buy_price = 850000
+  sell_price = 900000
+  if (current_price > sell_price) && (get_my_money("BTC")["amount"] > 0.001)
+    puts "売ります"
+    order("SELL", sell_price, 0.001)
+  end
+  elsif (current_price > buy_price) && (get_my_money("JPY")["amount"] > 1000)
+    puts "買います"
+    order("BUY", buy_price, 0.001)
+  else
+    puts "何もしません"
+  end
+
+  sleep(1)
+end
